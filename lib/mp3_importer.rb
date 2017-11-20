edbf6b5e9a9743006ccd54dc47092dcd09c526f0
Nolan Byrnes <nolanehs@gmail.com>
2017-11-20 11:56:05 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,8 +9,11 @@ def files
 @files=Dir.entries(@path).reject {|f| f =~ /\.{1,2}$/}
 end
 
-
-
-
+def import
+  self.files
+  @files.each do |filename|
+    Song.new_by_filename(filename)
+  end
+end
 
 end
