5df8811ebbae786710d617b12d00221530f20c56
Nolan Byrnes <nolanehs@gmail.com>
2017-11-20 11:43:47 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist
+attr_accessor :name, :songs
+@@all = []
+
+def initialize(name)
+@name=name
+@songs= []
+end
+
+def add_song(song)
+  @songs << song
+end
+
+def songs
+  @songs
+end
+
+
+
+
+end
