80b90296d196c9d3874af70d99cfa9c39bb42c9d
Nolan Byrnes <nolanehs@gmail.com>
2017-11-20 12:09:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,10 @@ def initialize(name)
   @name=name
 end
 
-
+def artist_name=(artist_name)
+ self.artist = Artist.find_or_create_by_name(artist_name)
+ artist.add_song(self)
+ end
 
 def self.new_by_filename(filename)
   song = self.new(filename.split(/\s-\s|\./)[1])
