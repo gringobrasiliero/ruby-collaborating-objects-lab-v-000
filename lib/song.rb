1954e418dabea56674dce3601dbdce7a724d48e8
Nolan Byrnes <nolanehs@gmail.com>
2017-11-20 11:48:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,9 +8,9 @@ end
 
 
 def self.new_by_filename(filename)
-  song=self.new
-  song.title = filename.split(" - ")[1]
-  song
-end
+  song = self.new(filename.split(/\s-\s|\./)[1])
+     song.artist_name = filename.split(/\s-\s|\./)[0]
+     song
+   end
 
 end
